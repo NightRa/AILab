324e0ac7abaf74e9426b0320791d6f9bf304bcec
NightRa <ilan3580@gmail.com>
2016-03-15 13:29:36 +0200

    Change representation of SelectionStrategy

--- Lab1/src/main/java/genetic/mating/ElitismMutationMateStrategy.java
+++ Lab1/src/main/java/genetic/mating/ElitismMutationMateStrategy.java
@@ -7,25 +7,27 @@ import scala.Tuple2;
 
 import java.util.Random;
 
-public class OnePointCrossover implements MateStrategy {
+public class ElitismMutationMateStrategy implements MateStrategy {
     public final double ElitismRate;
     public final double MutationRate;
     public final Random rand;
 
-    public OnePointCrossover(double elitismRate, double mutationRate, Random rand) {
+    public ElitismMutationMateStrategy(double elitismRate, double mutationRate, Random rand) {
         ElitismRate = elitismRate;
         MutationRate = mutationRate;
         this.rand = rand;
     }
 
     @Override
-    public <A> void mateStrategy(Genetic<A> alg, SelectionStrategy selection, Population<A> population, Population<A> buffer) {
+    public <A> void mateStrategy(Genetic<A> alg, SelectionStrategy selection, Population<A> population, Population<A> buffer, Population<A> parentsPool) {
         int popSize = population.population.length;
         int elites = (int) (popSize * ElitismRate);
         elitism(population, buffer, elites);
+        selection.populateParentsPool(population, parentsPool, rand);
         for (int i = elites; i < popSize; i++) {
-            Tuple2<A,A> parents = selection.selectParents(population, rand);
-            buffer.population[i].gene = alg.mate(parents._1, parents._2);
+            A parent1 = selection.chooseParent(parentsPool, rand);
+            A parent2 = selection.chooseParent(parentsPool, rand);
+            buffer.population[i].gene = alg.mate(parent1, parent2);
             if(rand.nextFloat() < MutationRate)
                 buffer.population[i].gene = alg.mutate(buffer.population[i].gene);
         }
