7b760f49a16e3870bf78b358aa3572f47d1e9045
YuvalAlf <u67v67@gmail.com>
2016-03-15 21:20:13 +0200

    Fix Knapsack mutation

--- Lab1/src/main/java/knapsack/KnapsackMutate.java
+++ Lab1/src/main/java/knapsack/KnapsackMutate.java
@@ -3,14 +3,14 @@ package knapsack;
 import scala.util.Random;
 
 public final class KnapsackMutate {
-    public void binomialMutate(KnapsackInstance instance, Random rand){
+    public void binomialMutate(KnapsackElement instance, Random rand){
         for (int i = 0; i < instance.amounts().length; i++)
             if (rand.nextBoolean())
                 instance.amounts()[i]++;
         instance.trim(rand);
     }
 
-    public void onePointMutate(KnapsackInstance instance, Random rand){
+    public void onePointMutate(KnapsackElement instance, Random rand){
         int index = rand.nextInt(instance.amounts().length);
         instance.amounts()[index]++;
         instance.trim(rand);
